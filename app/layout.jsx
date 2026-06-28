0af0deff1594152a2c233920ff2b486586721662
Hunter Master <210468292+Hunters-Marster@users.noreply.github.com>
2026-06-28 23:37:22 +0530

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "AniIndex",
+  description: "Discover Your Favourite Anime",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
